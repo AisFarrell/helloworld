b06dce97f8fba0468749623fe62f916252e6ac3c
AisFarrell <aslnfarrell@gmail.com>
2017-07-30 14:00:49 +0100

    hello world

---
+++ HelloWorldIntro/HelloWorldIntro.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldIntro.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
